7bf084d7879c0ccc2f1eb18935bd2138e2f98ff2
Kaspar Aun <114168113+hacker30083@users.noreply.github.com>
2026-02-17 22:14:54 +0200

    Update generate-data.mjs

--- generate-data.mjs
+++ generate-data.mjs
@@ -12,16 +12,7 @@ function buildBrowserHeaders(referer) {
 		"Accept": "*/*",
 		"Accept-Language": "en-GB,en;q=0.9,et-EE;q=0.8,et;q=0.7,en-US;q=0.6",
 		"Content-Type": "application/json; charset=UTF-8",
-		"Priority": "u=1, i",
-		"Sec-CH-UA": "\"Not:A-Brand\";v=\"99\", \"Google Chrome\";v=\"145\", \"Chromium\";v=\"145\"",
-		"Sec-CH-UA-Mobile": "?0",
-		"Sec-CH-UA-Platform": "\"Windows\"",
-		"Sec-Fetch-Dest": "empty",
-		"Sec-Fetch-Mode": "cors",
-		"Sec-Fetch-Site": "same-origin",
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
-		"X-Requested-With": "XMLHttpRequest",
-		"Referer": referer
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
 	};
 
 	return headers;
